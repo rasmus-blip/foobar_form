75d96bce621379c6e68ab0bc52cc3da2ea5e9afc
Mathias <math92k4@stud.kea.dk>
2021-05-26 12:27:20 +0200

    masking set up - check if the input.value is masked or unmasked

--- main.js
+++ main.js
@@ -3,10 +3,36 @@
 import "./sass/style.scss";
 import { setUpOrderForm } from "./order_form.js";
 import { setUpAccountCreation } from "./create_account.js";
+import Inputmask from "inputmask";
+// import restrictedInput from "restricted-input";
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  const cardNrMask = new Inputmask(`9999 9999 9999 9999`);
+  const exprDateMask = new Inputmask(`99/99`);
+  const cvvMask = new Inputmask(`999`);
+
+  const allCardNrInputs = document.querySelectorAll(`[name="card_number"]`);
+  allCardNrInputs.forEach((input) => {
+    cardNrMask.mask(input);
+  });
+
+  const allExprDateInputs = document.querySelectorAll(`[name="exp_date"]`);
+  allExprDateInputs.forEach((input) => {
+    exprDateMask.mask(input);
+  });
+
+  const allCvvInputs = document.querySelectorAll(`[name="cvv"]`);
+  allCvvInputs.forEach((input) => {
+    cvvMask.mask(input);
+  });
+
+  // const formattedCreditCardInput = new restrictedInput({
+  //   element: document.querySelector(`#account_form [name="card_number"]`),
+  //   pattern: "{{9999}} {{9999}} {{9999}} {{9999}}",
+  // });
+
   setUpOrderForm();
   setUpAccountCreation();
 }
